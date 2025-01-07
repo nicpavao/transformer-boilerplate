43b488c76bd9e03676c1aed5705cbb020951422a
nicpavao <mail@nicpavao.com>
2025-01-07 13:24:18 -0800

    added a data sequencer to encoder_only module

--- encoder_only.py
+++ encoder_only.py
@@ -1,9 +1,13 @@
 import torch
-from torch import nn
 
-def EncoderLayer():
-    return 
-
-def EncoderOnlyModel():
-    return
-
+# Define Dataset Class
+class SequenceDataset(torch.utils.data.Dataset):
+    def __init__(self, sequences, targets):
+        self.sequences = sequences
+        self.targets = targets
+    
+    def __len__(self):
+        return len(self.sequences)
+    
+    def __getitem__(self, idx):
+        return torch.tensor(self.sequences[idx], dtype=torch.long), torch.tensor(self.targets[idx], dtype=torch.long)
